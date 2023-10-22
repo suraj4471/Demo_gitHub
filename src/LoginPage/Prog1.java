77facc46bb3356faaf69e8525d777b61c860862d
suraj <suraj746@gmail.com>
2023-10-22 12:06:48 +0530

    first commit

---
+++ src/LoginPage/Prog1.java
@@ -0,0 +1,12 @@
+package LoginPage;
+
+public class Prog1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("enter user name");
+		
+		System.out.println("Enter Password");
+	}
+
+}
