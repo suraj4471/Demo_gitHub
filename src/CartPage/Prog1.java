186d18c8a341aa74e10566db4859ad7992d58cee
suraj <suraj746@gmail.com>
2023-10-23 09:05:40 +0530

    cart page

---
+++ src/CartPage/Prog1.java
@@ -0,0 +1,11 @@
+package CartPage;
+
+public class Prog1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is payment page");
+	}
+
+}
